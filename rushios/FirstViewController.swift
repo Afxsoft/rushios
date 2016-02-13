74a76634a656c2a571e9867ca4816be46859d53e
Afdal OULOUBE <aouloube@e1r11p5.42.fr>
2016-02-13 14:44:31 +0100

    Initial Commit

---
+++ rushios/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  rushios
+//
+//  Created by Afdal OULOUBE on 2/13/16.
+//  Copyright © 2016 Afdal OULOUBE. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
